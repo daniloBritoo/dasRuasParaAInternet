bfe720671308e280db6acb737d2fe68d7fa089a9
daniloBritoo <danilo.brito@sptech.school>
2023-06-10 20:20:50 -0300

    Telas de login e cadastro prontas

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -2,27 +2,14 @@ var mysql = require("mysql2");
 var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
-var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    },
-    options: {
-        encrypt: true, // for azure
-    }
-}
+
 
 // CONEXÃO DO MYSQL WORKBENCH
 var mySqlConfig = {
-    host: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    host: "localhost",
+    database: "projIndividual",
+    user: "root",
+    password: "1234",
 };
 
 function executar(instrucao) {
