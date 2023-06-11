1e6a9fe22dc04bd90cbb040b0a45e2e45e6a413a
daniloBritoo <danilo.brito@sptech.school>
2023-06-11 20:14:32 -0300

    Terminado 110%

--- addons/script.sql
+++ addons/script.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE projIndividual;
+se projIndividual;
 CREATE TABLE projIndividual.usuario(
 	id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45),
@@ -9,6 +9,7 @@ CREATE TABLE projIndividual.usuario(
 
 SELECT * FROM projIndividual.usuario;
 
+
 SELECT count(id) from projIndividual.usuario WHERE tipo = 'nova_guarda';
 SELECT count(id) from projIndividual.usuario WHERE tipo = 'Ambos';
 SELECT count(id) from projIndividual.usuario WHERE tipo = 'velha_guarda';
@@ -16,6 +17,7 @@ CREATE TABLE projIndividual.aviso (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	titulo VARCHAR(100),
 	tipo VARCHAR(45),
+    urlImg VARCHAR(1000),
 	fk_usuario INT,
 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 );
@@ -31,4 +33,20 @@ tipo Generos
     from usuario
         group by tipo;
         
-DROP TABLE aviso;
+select * from aviso;
+
+SELECT u.nome, a.fk_usuario, (SELECT count(id) FROM aviso ORDER BY fk_usuario) as qntd FROM usuario u JOIN aviso a ON a.fk_usuario = u.id;
+SELECT u.nome nomeUsuario, count(a.id) as qntdPosts FROM usuario u JOIN aviso a ON a.fk_usuario = u.id GROUP BY u.id ORDER BY qntdPosts desc limit 3;
+
+INSERT INTO projIndividual.usuario (nome, email, senha, tipo)
+VALUES
+    ('João Silva', 'joao.silva@example.com', '123456', 'nova_guarda'),
+    ('Maria Santos', 'maria.santos@example.com', 'abcdef', 'velha_guarda'),
+    ('Pedro Almeida', 'pedro.almeida@example.com', 'qwerty', 'Ambos'),
+    ('Ana Costa', 'ana.costa@example.com', 'password', 'nova_guarda'),
+    ('Lucas Oliveira', 'lucas.oliveira@example.com', 'abc123', 'velha_guarda'),
+    ('Carla Mendes', 'carla.mendes@example.com', '987654', 'Ambos'),
+    ('Marcos Pereira', 'marcos.pereira@example.com', 'senha123', 'nova_guarda'),
+    ('Fernanda Lima', 'fernanda.lima@example.com', 'abcd1234', 'velha_guarda'),
+    ('Rafaela Castro', 'rafaela.castro@example.com', 'p@ssw0rd', 'Ambos'),
+    ('Gustavo Souza', 'gustavo.souza@example.com', '12qwaszx', 'nova_guarda');
